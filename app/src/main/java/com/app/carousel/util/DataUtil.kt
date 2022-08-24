12c30314a1e598296029e0920df9e95194cff368
AkashGandh095 <gandhiakash8111@gmail.com>
2022-08-24 21:43:16 +0530

    changes

--- app/src/main/java/com/app/carousel/util/DataUtil.kt
+++ app/src/main/java/com/app/carousel/util/DataUtil.kt
@@ -8,34 +8,33 @@ import com.app.carousel.model.ImageData
 object DataUtil {
 
     val imageList = listOf(
-        R.drawable.img_10 , R.drawable.img_2 , R.drawable.img_4 , R.drawable.img_5,
-        R.drawable.img_6 , R.drawable.img_7 , R.drawable.img_8
+        R.drawable.ic_undraw_aircraft_re_m05i , R.drawable.ic_undraw_bitcoin_re_urgq , R.drawable.ic_undraw_delivery_truck_vt6p , R.drawable.ic_undraw_eiffel_tower_re_e11r,
+        R.drawable.ic_undraw_fireworks_re_2xi7 , R.drawable.ic_undraw_learning_sketching_nd4f , R.drawable.ic_undraw_proud_coder_re_exuy
     )
 
     val dataList = listOf(
-        ImageData("Playsam Streamliner" , R.drawable.img_10),
-        ImageData("Images API" , R.drawable.img_2),
-        ImageData("Wasa Crispbread" , R.drawable.img_4),
-        ImageData("Visual Representation" , R.drawable.img_5),
-        ImageData(" Electronic Display" , R.drawable.img_6),
-        ImageData("Image Label" , R.drawable.img_7),
-        ImageData("Carousal" , R.drawable.img_8),
-        ImageData("Data Pic" , R.drawable.img_7),
-        ImageData("Google" , R.drawable.img_8),
-        ImageData("Linkedin" , R.drawable.img_7),
-        ImageData("Youtube" , R.drawable.img_6),
-        ImageData("Lorem Ipsum" , R.drawable.img_5),
-        ImageData("dolor sit" , R.drawable.img_4),
-        ImageData("Consectetur Adipiscing" , R.drawable.img_2),
-        ImageData("Commodo Consequat" , R.drawable.img_6),
-        ImageData("Physical Exercise" , R.drawable.img_4),
-        ImageData("Nemo enim" , R.drawable.img_7),
-        ImageData("Neque porro" , R.drawable.img_8),
+        ImageData("Wandering  Mind" , R.drawable.ic_undraw_wandering_mind_re_x2a3),
+        ImageData("Stripe Payments" , R.drawable.ic_undraw_stripe_payments_re_chlm),
+        ImageData("Small Town" , R.drawable.ic_undraw_small_town_re_7mcn),
+        ImageData("Proud Coder" , R.drawable.ic_undraw_proud_coder_re_exuy),
+        ImageData("Off Road" , R.drawable.ic_undraw_off_road_re_leme),
+        ImageData("Mobile Encryption" , R.drawable.ic_undraw_mobile_encryption_re_yw3o),
+        ImageData("Learning Sketching" , R.drawable.ic_undraw_learning_sketching_nd4f),
+        ImageData("Fireworks" , R.drawable.ic_undraw_fireworks_re_2xi7),
+        ImageData("Eiffel Tower" , R.drawable.ic_undraw_eiffel_tower_re_e11r),
+        ImageData("Delivery Truck" , R.drawable.ic_undraw_delivery_truck_vt6p),
+        ImageData("BitCoin" , R.drawable.ic_undraw_bitcoin_re_urgq),
+        ImageData("AirCraft" , R.drawable.ic_undraw_aircraft_re_m05i),
+        ImageData("dolor sit" , R.drawable.ic_undraw_bitcoin_re_urgq),
+        ImageData("Consectetur Adipiscing" , R.drawable.ic_undraw_off_road_re_leme),
+        ImageData("Commodo Consequat" , R.drawable.ic_undraw_small_town_re_7mcn),
+        ImageData("Physical Exercise" , R.drawable.ic_undraw_wandering_mind_re_x2a3),
+        ImageData("Nemo enim" , R.drawable.ic_undraw_off_road_re_leme),
+        ImageData("Neque porro" , R.drawable.ic_undraw_fireworks_re_2xi7),
     )
 
 
     fun ImageView.loadImage(resId : Int , isCrossFade : Boolean = false , duration :Int = 0) {
-        scaleType = ImageView.ScaleType.CENTER_CROP
         this.load(resId) {
             this.crossfade(isCrossFade)
             crossfade(duration)
